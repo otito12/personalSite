18fefe446b1c9bfaed06d7e4315b5c83604066f9
Otitodirichukwu <oodarluz@syr.edu>
2021-01-07 15:50:22 -0500

    navigation bar complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { SocialMediaBarComponent } from './social-media-bar/social-media-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    LandingPageComponent,
+    SocialMediaBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/social-media-bar/social-media-bar.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-social-media-bar',
+  templateUrl: './social-media-bar.component.html',
+  styleUrls: ['./social-media-bar.component.css']
+})
+export class SocialMediaBarComponent implements OnInit {
+
+  viewBox = '0 0 25 25';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
